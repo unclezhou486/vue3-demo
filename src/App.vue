044c19cffae53db50859dcbcc6e32fd4a509e36d
unclezhou486 <1543130456@qq.com>
2024-03-26 20:21:27 +0800

    初步优化

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
   import {ref} from "vue";
 
   let queryInput = ref ("")
-  let tableData = ref ([{
+  let tableData = ref ([
+      {
     date: '2016-05-03',
     name: 'Tom',
     state: 'California',
@@ -39,10 +40,27 @@
       zip: 'CA 90036',
       tag: 'Office',
     }])
+  let multipleSelection = ref ([])
+  let dialogFormVisible = ref(false)
+  let tableForm = ref (
+      {
+        name:'',
+        email:"",
+        address:""
+      }
+  )
 const handleRowClick = () => {
     console.log('click')
 }
 
+  const handleSelectionChange = (val) => {
+    multipleSelection.value = val
+    console.log(val)
+  }
+
+  const handleAdd = () =>{
+    dialogFormVisible.value = true
+  }
 
 </script>
 
@@ -55,13 +73,21 @@ const handleRowClick = () => {
 <!-- query   -->
     <div class="query-box">
 <!-- input-->
-      <el-input v-model="input" style="width: 240px" placeholder="queryInput" />
+      <el-input v-model="queryInput" style="width: 240px" placeholder="请输入姓名搜索" />
 <!-- button-->
+      <el-button type="primary" @click="handleAdd">增加</el-button>
     </div>
     <div>
-      <el-button type="primary">Primary</el-button>
+
     </div>
-    <el-table :data="tableData" style="width: 100%">
+    <el-table
+        border
+        ref="multipleTableRef"
+        :data="tableData"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection" width="55" />
       <el-table-column fixed prop="date" label="Date" width="150" />
       <el-table-column prop="name" label="Name" width="120" />
       <el-table-column prop="state" label="State" width="120" />
@@ -75,6 +101,28 @@ const handleRowClick = () => {
         </template>
       </el-table-column>
     </el-table>
+
+<!--dialog-->
+    <el-dialog v-model="dialogFormVisible" title="Shipping address" width="500">
+      <el-form :model="tableForm">
+        <el-form-item label="请输入姓名" :label-width="60">
+          <el-input v-model="tableForm.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="请输入邮箱" :label-width="60">
+          <el-input v-model="tableForm.email" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="请输入地址" :label-width="60">
+          <el-input v-model="tableForm.address" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="dialogFormVisible = false">
+            确认
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -85,6 +133,15 @@ const handleRowClick = () => {
   top: 30%;
   left: 30%;
   treansform: translate(-50%,-50%);
-
+}
+.title{
+  text-algin:center;
+}
+.query-box{
+  display: flex;
+  justify-content: space-between;
+}
+.el-input{
+  width:
 }
 </style>
